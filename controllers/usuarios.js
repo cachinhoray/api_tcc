9eda254dd039dd85b54ba03825939447beaa793b
cachinhoray <rayssacavalcante952@gmail.com>
2024-08-19 11:43:22 -0300

    aula 2 19/08

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -16,4 +16,55 @@ module.exports = {
             });
         }
     }, 
+
+
+    async cadastrarUsuario(request, response) {
+        try {            
+            return response.status(200).json({
+                sucesso: true, 
+                mensagem: 'Cadastro usuario.', 
+                dados: null
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro na requisição.',
+                dados: error.message
+            });
+        }
+    }, 
+
+
+    async editarUsuarios(request, response) {
+        try {            
+            return response.status(200).json({
+                sucesso: true, 
+                mensagem: 'editar  usuários.', 
+                dados: null
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro na requisição.',
+                dados: error.message
+            });
+        }
+    }, 
+
+
+    async apagarUsuarios(request, response) {
+        try {            
+            return response.status(200).json({
+                sucesso: true, 
+                mensagem: 'apagar s usuários.', 
+                dados: null
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro na requisição.',
+                dados: error.message
+            });
+        }
+    }, 
 };  
